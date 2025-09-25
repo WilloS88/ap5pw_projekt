4465ff4545902d2742261f8e644a9eb48f68e98d
WilloS88 <ramon2003@email.cz>
2025-09-25 14:47:47 +0200

    Init

---
+++ AP5PW_Helpdesk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AP5PW_Helpdesk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
