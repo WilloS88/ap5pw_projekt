c075a3246a2fad9de9f8ffcfef48e6acf3222e4c
Roman Vilčák <ramon2003@email.cz>
2025-10-22 13:35:53 +0200

    Merge conflicts fixed

--- AP5PW_Helpdesk/ViewModels/UserVM.cs
+++ AP5PW_Helpdesk/ViewModels/UserVM.cs
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-﻿using System;
-=======
-﻿    using System;
->>>>>>> 6432ee709d2c5dafc8d6df14538df5b017f4a644
 using System.ComponentModel.DataAnnotations;
-
 namespace AP5PW_Helpdesk.ViewModels
 {
     public class UserVM
     {
         public int Id { get; set; }
 
-<<<<<<< HEAD
         [Required, StringLength(50)]
         public string Username { get; set; } = "";
 
@@ -32,24 +25,5 @@ namespace AP5PW_Helpdesk.ViewModels
 
         [DataType(DataType.Password)]
         public string? Password { get; set; }
-=======
-        [Required, StringLength(50)] 
-        public string Username          { get; set; } = "";
-
-        [StringLength(50)] 
-        public string LastName          { get; set; } = "";
-
-        [Required, StringLength(30)] 
-        public string Nickname          { get; set; } = "";
-
-        [Required, EmailAddress] 
-        public string Email             { get; set; } = "";
-        
-        [Required]
-        public int RoleId               { get; set; }
-
-        [Required]
-        public string RoleName          { get; set; } = "";
->>>>>>> 6432ee709d2c5dafc8d6df14538df5b017f4a644
     }
 }
