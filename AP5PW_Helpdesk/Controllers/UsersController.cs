6432ee709d2c5dafc8d6df14538df5b017f4a644
Roman Vilčák <ramon2003@email.cz>
2025-10-22 12:04:44 +0200

    Načítání dat z db

--- AP5PW_Helpdesk/Controllers/UsersController.cs
+++ AP5PW_Helpdesk/Controllers/UsersController.cs
@@ -1,22 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using AP5PW_Helpdesk.Models;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using AP5PW_Helpdesk.Data;
+using AP5PW_Helpdesk.ViewModels;
 
 namespace AP5PW_Helpdesk.Controllers
 {
-    public class UsersController : Controller
-    {
-        public IActionResult Index()
-        {
-            // dočasný fake data, jen aby tabulka něco zobrazila
-            var demo = new List<User> {
-                new User { Id=1, FirstName="Jan", LastName="Novák", Nickname="jnovak", Email="jan@firma.cz" },
-                new User { Id=2, FirstName="Petr", LastName="Dvořák", Nickname="pdvorak", Email="petr@firma.cz" }
-            };
-            return View(demo); // Views/Users/Index.cshtml
-        }
+	public class UsersController : Controller
+	{
+		private readonly AppDbContext _db;
 
-        public IActionResult Create() => View();       // Views/Users/Create.cshtml
-        public IActionResult Edit(int id) => View();    // Views/Users/Edit.cshtml
-        public IActionResult Details(int id) => View(); // Views/Users/Details.cshtml
-    }
+		public UsersController(AppDbContext db)
+		{
+			_db = db;
+		}
+
+		// GET: /Users
+		public async Task<IActionResult> Index()
+		{
+			var vm = await _db.Users
+				.AsNoTracking()
+				.Include(u => u.Role)                     // kvůli RoleName
+				.OrderBy(u => u.Username)
+				.Select(u => new UserVM
+				{
+					Id = u.Id,
+					Username = u.Username,
+					RoleId = u.RoleId,
+					RoleName = u.Role != null ? u.Role.Name : ""
+				})
+				.ToListAsync();
+
+			return View(vm); // Views/Users/Index.cshtml -> IEnumerable<UserDto>
+		}
+
+		// Zatím jen prázdné scaffolding akce (můžeš doplnit později)
+		public IActionResult Create() => View();
+		public IActionResult Edit(int id) => View();
+		public IActionResult Details(int id) => View();
+	}
 }
---
+++ AP5PW_Helpdesk/ViewModels/UserVM.cs
@@ -0,0 +1,28 @@
+﻿    using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AP5PW_Helpdesk.ViewModels
+{
+    public class UserVM
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)] 
+        public string Username          { get; set; } = "";
+
+        [StringLength(50)] 
+        public string LastName          { get; set; } = "";
+
+        [Required, StringLength(30)] 
+        public string Nickname          { get; set; } = "";
+
+        [Required, EmailAddress] 
+        public string Email             { get; set; } = "";
+        
+        [Required]
+        public int RoleId               { get; set; }
+
+        [Required]
+        public string RoleName          { get; set; } = "";
+    }
+}
