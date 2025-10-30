3b38b442ca8aff2de5ac658b07b890a58af4a68e
Roman Vilčák <ramon2003@email.cz>
2025-10-30 16:06:04 +0100

    Test

--- AP5PW_Helpdesk/Entities/User.cs
+++ AP5PW_Helpdesk/Entities/User.cs
@@ -6,6 +6,7 @@
 		public string UserName				{ get; set; } = "";
 		public string LastName				{ get; set; } = "";
 		public string Password				{ get; set; } = "";
+		public string PasswordHash			{ get; set; } = "";
 		public int RoleId					{ get; set; }
 		public Role Role					{ get; set; } = null!;
 		public int? CompanyId				{ get; set; }
